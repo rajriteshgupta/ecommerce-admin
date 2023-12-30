b991f717ae234ba86dedc4e177c128708e52071a
Ritesh Raj <rajriteshkatihar@gmail.com>
2023-12-30 16:03:01 +0530

    Admin "dashboard" and "settings" setup

---
+++ components/ui/heading.tsx
@@ -0,0 +1,16 @@
+interface HeaderingProps {
+    title: string;
+    description: string;
+}
+
+export const Heading: React.FC<HeaderingProps> = ({
+    title,
+    description
+}) => {
+    return(
+        <div>
+            <h2 className="text-3xl font-bold tracking-light">{title}</h2>
+            <p className="text-sm text-muted-foreground">{description}</p>
+        </div>
+    );
+}
